818a22c3cef627d705b68768e9ef53ba402385cb
PatDahl79 <pattaravarat.dahl@chasacademy.se>
2024-05-23 16:17:32 +0200

    ,,
    ,,

---
+++ myblogs/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default Home
